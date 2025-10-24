2da253940f1ed6bfbae765e66284e0ae3ee8bd35
Lucas Papadam <lucaspapadam3@gmail.com>
2025-10-24 16:55:09 +1100

    updated files 24/10/2025

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
-echo "macOS(1) or Arch[outdated]?(2)"
+echo "macOS(1) or Arch?(2)"
 read x
+#
+#
+#
+#
+# MACOS
+#
+#
+#
+#
 if [ "$x" == "1" ]; then
   clear
   echo "macos"
@@ -39,7 +48,14 @@ if [ "$x" == "1" ]; then
   cd /Users/$USER/
   rm -rf /Users/$USER/script/
 fi
-
+#
+#
+#
+#
+# ARCH LINUX
+#
+#
+#
 if [ "$x" == "2" ]; then
   yes | sudo cp -r pacman.conf /etc/
   yes | sudo pacman -Sy
@@ -49,28 +65,26 @@ if [ "$x" == "2" ]; then
   clear
   echo "Installing dependencies"
   sleep 1 
-  sudo pacman -S hyprland hyprpaper ark neovim obs-studio qbittorrent dolphin kitty pavucontrol rofi waybar swaync flatpak github-cli zsh kdenlive blender mpv feh qemu-full lutris steam dolphin-emu ttf-hack-nerd 
-  flatpak install flathub io.gitlab.librewolf-community -y
-  flatpak install flathub com.discordapp.Discord -y
-  flatpak install flathub org.prismlauncher.PrismLauncher -y
-  flatpak install flathub net.kuribo64.melonDS -y
-  flatpak install flathub info.cemu.Cemu -y
-  flatpak install flathub io.github.gopher64.gopher64 -y
-  flatpak install flathub io.github.ungoogled_software.ungoogled_chromium -y
+  sudo pacman -S hyprland hyprpaper ark neovim obs-studio qbittorrent dolphin alacritty pavucontrol rofi waybar swaync flatpak github-cli zsh kdenlive blender mpv feh qemu-full lutris ttf-hack-nerd 
+  clear
+  echo "Installing Yay"
+  yes | sudo pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si
+
+  #flatpak install flathub io.gitlab.librewolf-community -y
+  #flatpak install flathub com.discordapp.Discord -y
+  #flatpak install flathub org.prismlauncher.PrismLauncher -y
+  #flatpak install flathub net.kuribo64.melonDS -y
+  #flatpak install flathub info.cemu.Cemu -y
+  #flatpak install flathub io.github.gopher64.gopher64 -y
+  #flatpak install flathub io.github.ungoogled_software.ungoogled_chromium -y
   clear
   echo "Installing NvChad"
 # Install NvChad
   git clone https://github.com/NvChad/starter ~/.config/nvim
-  cd /home/$USER/
-  git clone https://github.com/lucaspapadam/centauri.git
-  cd /home/$USER/centauri/stuff/
-  cp -R ./hypr/ /home/$USER/.config
-  cp -R ./kitty/ /home/$USER/.config
-  cp -R ./rofi/ /home/$USER/.config
-  cp -R ./waybarconfig /home/$USER/.config/waybar
-  sudo cp -R ./swaync /etc/xdg/
-  sudo cp -R ./waybar/ /etc/xdg/
-  rm -rf /home/$USER/centauri/
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
