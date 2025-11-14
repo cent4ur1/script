47fc70c10dbd19cb41363ae726e910e754530b52
Lucas Papadam <lucaspapadam3@gmail.com>
2025-11-14 18:19:53 +1100

    updated files 14/11/2025

--- arch.sh
+++ arch.sh
@@ -2,6 +2,7 @@ cp -R ./files/ ~/.config/
 rm -rf $HOME/.config/audacious/
 rm -rf $HOME/.config/sketchybar/
 rm -rf $HOME/.config/borders/
+cp ./files/.zshrc $HOME
 
 sudo pacman -S alacritty qbittorrent 
 
