0acd7f347355aaaa5a31b6c7511421e1f4be6baa
Lucas Papadam <lucaspapadam3@gmail.com>
2025-11-19 11:44:07 +1100

    updated files 19/11/2025

--- dotfilesupdate.sh
+++ dotfilesupdate.sh
@@ -1,6 +1,7 @@
 cp -r $HOME/.config/ /Users/$USER/script/files/
 cp $HOME/.aerospace.toml /Users/$USER/script/files/
 cp $HOME/.zshrc /Users/$USER/script/files/
+cp $HOME/.mpd /Users/$USER/script/files/
 cd $HOME/script/
 git add .
 git commit -m "updated files $(date +%d/%m/%Y)"
