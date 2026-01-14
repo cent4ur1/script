b00c9cd032a5d362423478f1e640b321b209230f
cent4ur1 <lucaspapadam3@gmail.com>
2026-01-15 08:58:35 +1100

    updated files 15/01/2026

--- arch.sh
+++ arch.sh
@@ -3,7 +3,7 @@ cp ./dotfilesupdatearch.sh $HOME/
 cp ./archfiles/leverup.opus $HOME/
 cp ./archfiles/leverdown.opus $HOME/
 cp ./archfiles/mute.sh $HOME/
-sudo pacman -S alacritty firefox zsh thunar yazi mpv obs-studio ffmpeg wl-clipboard yt-dlp flatpak btop ttf-hack-nerd github-cli hyprland hyprpaper hyprlock hyprlauncher waybar waybar pavucontrol thunar
+sudo pacman -S alacritty firefox zsh thunar yazi mpv obs-studio ffmpeg wl-clipboard yt-dlp flatpak btop ttf-hack-nerd github-cli hyprland hyprpaper hyprlock hyprlauncher waybar waybar pavucontrol thunar wlsunset
 
 git clone https://github.com/NvChad/starter ~/.config/nvim
 
@@ -11,6 +11,10 @@ flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub org.qbittorrent.qBittorrent -y
 
 chsh -s /usr/bin/zsh
+
+chmod +x $HOME/clean.sh 
+chmod +x $HOME/mute.sh
+chmod +x $HOME/dotfilesupdatearch.sh 
 
 cp -R ./archfiles/alacritty/ $HOME/.config
 cp -R ./archfiles/hypr/ $HOME/.config
