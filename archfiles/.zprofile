a2e71691d3a150dec03fd3403185c35a35cdd4f3
cent4ur1 <lucaspapadam3@gmail.com>
2026-01-15 08:59:28 +1100

    updated files 15/01/2026

---
+++ archfiles/.zprofile
@@ -0,0 +1,2 @@
+start-hyprland
+#startplasma-wayland
---
+++ archfiles/dotfilesupdatearch.sh
@@ -0,0 +1,14 @@
+cp -R $HOME/.config/hypr/ $HOME/script/archfiles/
+cp -R $HOME/.config/waybar/ $HOME/script/archfiles/
+cp -R $HOME/.config/alacritty/ $HOME/script/archfiles/
+cp -R $HOME/.config/alacritty/ $HOME/script/archfiles/
+cp -R $HOME/.config/btop/ $HOME/script/archfiles/
+cp -R $HOME/.zshrc $HOME/script/archfiles/
+cp -R $HOME/.zprofile $HOME/script/archfiles
+cp -R $HOME/dotfilesupdatearch.sh $HOME/script/archfiles
+
+cd $HOME/script/
+git add .
+git commit -m "updated files $(date +%d/%m/%Y)"
+git push
+
