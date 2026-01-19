df5b1ed88f82b5a8147f532f6ccbbb958b329168
cent4ur1 <lucaspapadam3@gmail.com>
2026-01-19 22:24:35 +1100

    updated files 19/01/2026

--- archfiles/dotfilesupdatearch.sh
+++ archfiles/dotfilesupdatearch.sh
@@ -4,6 +4,7 @@ cp -R $HOME/.config/alacritty/ $HOME/script/archfiles/
 cp -R $HOME/.config/alacritty/ $HOME/script/archfiles/
 cp -R $HOME/.config/btop/ $HOME/script/archfiles/
 cp -R $HOME/.zshrc $HOME/script/archfiles/
+cp -R $HOME/.config/mpd/ $HOME/script/archfiles/
 cp -R $HOME/.zprofile $HOME/script/archfiles/
 cp -R $HOME/dotfilesupdatearch.sh $HOME/script/archfiles/
 cp -R $HOME/clean.sh $HOME/script/archfiles/
