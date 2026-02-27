74826dd9ae6183e9e27e33138000fcf7d206d0e5
Lucas Papadam <lucaspapadam3@gmail.com>
2026-02-27 15:19:40 +1100

    updated files 27/02/2026

--- macos/start.sh
+++ macos/start.sh
@@ -6,26 +6,27 @@ clear
 echo "[1] Installing brew"
 sleep 1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-yes | brew install autoconf automake boost borders btop ca-certificates cairo cmake fastfetch fftw fontconfig freetype gcc gettext gh giflib git glib gmp graphite2 harfbuzz hwloc icu4c@77 iproute2mac isl jpeg-turbo libevent libmpc libmpdclient libpng libtiff libtool libunistring libuv libx11 libxau libxcb libxdmcp libxext libxrender little-cms2 lpeg luajit luv lz4 lzo m4 molten-vk mpdecimal mpfr nasm ncmpcpp ncurses neovim ninja open-mpi openjdk openssl@3 p7zip pcre2 pixman pmix python@3.13 readline sevenzip sketchybar sqlite taglib tree-sitter unibilium utf8cpp utf8proc xorgproto xz yazi yyjson zstd aerospace alacritty balenaetcher discord firefox font-hack-nerd-font gimp heroic kdenlive localsend luanti monitorcontrol mullvad-vpn obs obsidian prismlauncher ungoogled-chromium 
+yes | brew install autoconf automake boost borders btop ca-certificates cairo cmake fastfetch fftw fontconfig freetype gcc gettext gh giflib git glib gmp graphite2 harfbuzz hwloc icu4c@77 iproute2mac isl jpeg-turbo libevent libmpc libmpdclient libpng libtiff libtool libunistring libuv libx11 libxau libxcb libxdmcp libxext libxrender little-cms2 lpeg luajit luv lz4 lzo m4 molten-vk mpdecimal mpfr nasm ncmpcpp ncurses neovim ninja open-mpi openjdk openssl@3 p7zip pcre2 pixman pmix python@3.13 readline sevenzip sketchybar sqlite taglib tree-sitter unibilium utf8cpp utf8proc xorgproto xz yazi yyjson zstd aerospace alacritty balenaetcher discord firefox font-hack-nerd-font gimp heroic kdenlive localsend luanti monitorcontrol mullvad-vpn obs obsidian prismlauncher steam ungoogled-chromium 
 clear
-echo "[3] Install extra packages? 1[yes] 2[no]"
-echo "Discord, PrismLauncher, Dolphin, Ares-emulator"
+echo "[3] Install extra packages? 1[yes] 2[no]\nDiscord, PrismLauncher, Dolphin, Ares-emulator"
 read a
 if [ "$a" == "1" ]; then
   yes | brew install discord prismlauncher dolphin ares-emulator
-fi
-clear
-echo "[4] Setting defaults"
-mv ./files/.aerospace.toml $HOME/ 
-mv ./files/.zshrc $HOME/ 
-mkdir $HOME/.config
-mv ./files/* $HOME/.config/ 
-./macosdefaults.sh
+else
+  clear
+  echo "[4] Setting defaults"
+  mv ./files/.aerospace.toml $HOME/ 
+  mv ./files/.zshrc $HOME/ 
+  mkdir $HOME/.config
+  mv ./files/* $HOME/.config/ 
+  ./macosdefaults.sh
 
-echo "Done"
-open /Applications/AeroSpace.app/
-open /Applications/MonitorControl.app/
-sleep 2 
-clear
-cd /Users/$USER/
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "Done"
+  open /Applications/AeroSpace.app/
+  open /Applications/MonitorControl.app/
+  sleep 2 
+  clear
+  cd /Users/$USER/
+  #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
