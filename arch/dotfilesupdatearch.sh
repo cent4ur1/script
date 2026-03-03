349cad8178b562695929fbe9f0c495fcb16eb52c
Lucas Papadam <lucaspapadam3@gmail.com>
2026-03-03 21:55:30 +0000

    updated files 03/03/2026

--- arch/dotfilesupdatearch.sh
+++ arch/dotfilesupdatearch.sh
@@ -1,16 +1,14 @@
-cp -R $HOME/.config/hypr/ $HOME/script/archfiles/
-cp -R $HOME/.config/waybar/ $HOME/script/archfiles/
-cp -R $HOME/.config/alacritty/ $HOME/script/archfiles/
-cp -R $HOME/.config/alacritty/ $HOME/script/archfiles/
-cp -R $HOME/.config/btop/ $HOME/script/archfiles/
-cp -R $HOME/.zshrc $HOME/script/archfiles/
-cp -R $HOME/.zprofile $HOME/script/archfiles/
-cp -R $HOME/dotfilesupdatearch.sh $HOME/script/archfiles/
-cp -R $HOME/clean.sh $HOME/script/archfiles/
-echo "sudo pacman -S" $(pacman -Qqe) > $HOME/script/packages.sh
+cp -R $HOME/.config/hypr/ $HOME/script/arch/files
+cp -R $HOME/.config/waybar/ $HOME/script/arch/files
+cp -R $HOME/.config/alacritty/ $HOME/script/arch/files
+cp -R $HOME/.config/btop/ $HOME/script/arch/files
+cp -R $HOME/.zshrc $HOME/script/arch/files
+cp -R $HOME/dotfilesupdatearch.sh $HOME/script/arch/
+cp -R $HOME/clean.sh $HOME/script/arch/
+cp -R $HOME/.local/share/icons/ $HOME/script/arch/files
+echo "sudo pacman -S" $(pacman -Qqe) >>$HOME/script/arch/files/arch.sh
 
 cd $HOME/script/
 git add .
 git commit -m "updated files $(date +%d/%m/%Y)"
 git push
-
